ca2089b55aff740d31643b171c4b239b06b97738
ivonerkov <94400396+ivonerkov@users.noreply.github.com>
2023-06-18 00:59:11 +0200

    Delete src directory

--- src/main/java/com/example/ikt_project/model/exceptions/InvalidArgumentsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ikt_project.model.exceptions;
-
-public class InvalidArgumentsException extends RuntimeException{
-    public InvalidArgumentsException() {
-        super("Invalid arguments exception");
-    }
-}
--- src/main/java/com/example/ikt_project/model/exceptions/PasswordsDoNotMatchException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ikt_project.model.exceptions;
-
-public class PasswordsDoNotMatchException extends RuntimeException {
-    public PasswordsDoNotMatchException() {
-        super("Passwords do not match exception.");
-    }
-}
